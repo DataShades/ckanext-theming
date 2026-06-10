6e79516d07ee5d4de3e92986ed7fbaacba20814d
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2026-06-10 10:19:41 +0300

    chore: fix bundling

--- ckanext/theming/themes/classic/vite.config.ts
+++ ckanext/theming/themes/classic/vite.config.ts
@@ -3,7 +3,18 @@ import { resolve } from "path";
 
 export default defineConfig({
   publicDir: false,
-
+  plugins: [
+    {
+      name: "prepend-semicolon",
+      generateBundle(options, bundle) {
+        for (const chunk of Object.values(bundle)) {
+          if (chunk.type === "chunk") {
+            chunk.code = ";" + chunk.code + "\n";
+          }
+        }
+      },
+    },
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "assets/src/main.ts"),
